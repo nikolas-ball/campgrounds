38ca089bb6220561da804ec28f3a0e90d82a63ef
Nick <ballnikolas@yahoo.com>
2020-02-17 14:23:59 +0000

    v8 mod & v9 init

--- v8/routes/campgrounds.js
+++ v8/routes/campgrounds.js
@@ -15,7 +15,7 @@ router.get("/", function(req, res){
 });
 
 // CREATE - add new campground to DB
-router.post("/", function(req, res){
+router.post("/", isLoggedIn, function(req, res){
 	// res.send("you hit the post route")
 	//get data from form and add to campgrounds array
 	var name = req.body.name;
@@ -35,7 +35,7 @@ router.post("/", function(req, res){
 });
 
 // NEW - show form to create new campground
-router.get("/new", function(req, res){
+router.get("/new", isLoggedIn, function(req, res){
 	res.render("campgrounds/new");
 });
 
@@ -52,5 +52,13 @@ router.get("/:id", function(req, res){
 		}
 	});
 });
+
+//middleware
+function isLoggedIn(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	}
+	res.redirect("/login");
+}
 
 module.exports = router;
