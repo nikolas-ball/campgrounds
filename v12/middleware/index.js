b1a729eba4e4c5defcb428a368b767b334c0751f
Nick <ballnikolas@yahoo.com>
2020-02-28 22:36:03 +0000

    first v12 commit

---
+++ v12/middleware/index.js
@@ -0,0 +1,58 @@
+var Campground = require("../models/campground");
+var Comment = require("../models/comment");
+//all the middleware goes here
+var middlewareObj = {};
+
+middlewareObj.checkCampgroundOwnership = function(req, res, next) {
+	if(req.isAuthenticated()){
+		Campground.findById(req.params.id, function(err, foundCampground){
+			if(err || !foundCampground){
+				req.flash("error", "Campground Not Found!");
+				res.redirect("back");
+			} else {
+				//does user own the campground
+				if(foundCampground.author.id.equals(req.user._id)) {
+					next();
+				} else {
+					req.flash("error", "You Do Not Have Permission To Do That");
+					res.redirect("back");
+				}
+			}
+		});
+	} else {
+		req.flash("error", "You Need To Be Logged In To Do That");
+		res.redirect("back");
+	}
+}
+
+middlewareObj.checkCommentOwnership = function(req, res, next) {
+	if(req.isAuthenticated()){
+		Comment.findById(req.params.comment_id, function(err, foundComment){
+			if(err || !foundComment){
+				req.flash("error", "Comment Not Found");
+				res.redirect("back");
+			} else {
+				//does user own the comment
+				if(foundComment.author.id.equals(req.user._id)) {
+					next();
+				} else {
+					req.flash("error", "You Do Not Have Permission To Do That");
+					res.redirect("back");
+				}
+			}
+		});
+	} else {
+		req.flash("error", "You Need To Be Logged In To Do That");
+		res.redirect("back");
+	}
+}
+
+middlewareObj.isLoggedIn = function(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	}
+	req.flash("error", "You Need To Be Logged In To Do That");
+	res.redirect("/login");
+}
+
+module.exports = middlewareObj
