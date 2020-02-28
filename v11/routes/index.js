4e8a1e051502d6d3f71d85ba61277fc713f527b2
Nick <ballnikolas@yahoo.com>
2020-02-28 00:39:23 +0000

    v11 commit

--- v11/routes/index.js
+++ v11/routes/index.js
@@ -41,6 +41,7 @@ router.post("/login", passport.authenticate("local",
 //logout route
 router.get("/logout", function(req, res){
 	req.logout();
+	req.flash("success", "Logged You Out");
 	res.redirect("/campgrounds");
 });
 
